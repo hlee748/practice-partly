63fbe935f537b90d723175521d8c2f85ff93dc7c
julie <hlee748@gmail.com>
2021-11-25 00:31:35 +0900

    nav bar

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,9 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div >
+    <nav>
+    <div id="navbar">
+      <div className="logo"><a href="#">DONUT</a></div>
+        <div className="nav-links">
+          <li><a href="#">HOME</a></li>
+          <li>
+            <a href="#">HTML & CSS</a>
+            <ul className="htmlCss-sub-menu sub-menu">
+              <li><a href="#">Web Design</a></li>
+              <li><a href="#">Login Form</a></li>
+              <li><a href="#">Card Design</a></li>
+              <li><a href="#">More</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">JAVASCRIPT</a>
+            <ul className="js-sub-menu sub-menu">
+              <li><a href="#">Dynamic Calculator</a></li>
+              <li><a href="#">Form Validation</a></li>
+              <li><a href="#">Calendar</a></li>
+              <li><a href="#">Digital Clock</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">ABOUT US</a>
+            <ul className="htmlCss-sub-menu sub-menu">
+              <li><a href="#">Web Design</a></li>
+              <li><a href="#">Login Form</a></li>
+              <li><a href="#">Card Design</a></li>
+              <li><a href="#">More</a></li>
+            </ul>
+          </li>
+      </div>
 
+     
     </div>
+    </nav>
   );
 }
 
